40f1dcde20d23e74b94b4d5f46e93f13beb48c8a
kombarar <39060163+kombarar@users.noreply.github.com>
2021-02-14 22:36:39 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,4 @@
+print("Anyone who")
+print("          stops")
+print("               learning s old,")
+print("   whether at twenty or eighty")
---
+++ test.py
@@ -0,0 +1,3 @@
+print("Nothing will"+"\n"+"work unless you do")
+print("Nothing will")
+print("work unless you do")
