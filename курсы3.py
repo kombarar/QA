d05a36b6a3b14d93fce8b5f488dee443a588f00a
kombarar <39060163+kombarar@users.noreply.github.com>
2021-02-20 22:38:29 +0700

    Add files via upload

---
+++ курсы3.py
@@ -0,0 +1,7 @@
+proffesion1 = input ("Введите профессию #1 ")
+proffesion2 = input ("Введите профессию #2 ")
+proffesion3 = input ("Введите профессию #3 ")
+predlog = input ("Введите чем их разделить ")
+print("Кто он?")
+print("Он - "+ proffesion1 + " " + predlog + " " + proffesion2 + " " + predlog + " " + proffesion3)
+
---
+++ питон3.1.py
@@ -0,0 +1,4 @@
+time = input("Введите кол-во минут" )
+hours = int(time) / 60
+sec = int(time) * 60
+print("Это " + str(hours) + " часов и " + str(sec) + " секунд")
