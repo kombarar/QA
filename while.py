2ab58b6ddf3b6f4e462cece80873b8a0d16de2e8
kombarar <39060163+kombarar@users.noreply.github.com>
2021-03-02 01:18:25 +0700

    Add files via upload

---
+++ while.py
@@ -0,0 +1,19 @@
+x = int(input("Введите 1 число: "))
+
+y = int(input("Введите 2 число: "))
+
+#for i in range(a, b+1):
+
+#   print(i, end=' ')
+
+
+#for i in range(a, b):
+
+ #  if i % 2 != 0:
+
+  #     print(i, end=' ')
+
+
+while y >= x:
+    print(y, end=' ')
+    y = y-1
