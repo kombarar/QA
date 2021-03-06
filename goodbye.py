7cd2cb7735aa9c42d7efb20a1fceeef6a050380e
kombarar <39060163+kombarar@users.noreply.github.com>
2021-03-07 00:49:10 +0700

    Add files via upload

---
+++ goodbye.py
@@ -0,0 +1,12 @@
+while True:
+    a = int(input("Введите целое число: "))
+    if a == 7:
+        print("Good bye!")
+        break
+    if a > 0:
+        print("Number is positive")
+    if a < 0:
+            print("Number is negative")
+    if a == 0:
+        print("Number is equal to zero")
+
