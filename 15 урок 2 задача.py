3a63e43baddb5c2a7d7c03b4e4fa8767425de07f
kombarar <39060163+kombarar@users.noreply.github.com>
2021-03-30 00:48:56 +0700

    Add files via upload

--- 15 урок 2 задача.py
+++ 15 урок 2 задача.py
@@ -1,9 +1,10 @@
-
+dlina = int(input())
+napravl = input()
+simvol = input()
 def fnc(dlina, napravl, simvol):
     for i in range(1, dlina):
         if napravl =="вертикально":
             print(simvol)
         if napravl =="горизонтально":
             print(simvol, end=' ')
-fnc(10,"горизонтально", "*")
-
+fnc(dlina, napravl, simvol)
